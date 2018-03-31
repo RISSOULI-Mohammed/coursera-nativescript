acd3921516d98f829befa424d275e0580c7c833b
RISSOULI-Mohammed <simo.rissouli@hotmail.com>
2018-03-31 19:46:50 +0100

    Social Sharing

--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -17,6 +17,8 @@ import { View } from "ui/core/view";
 import { SwipeGestureEventData, SwipeDirection } from "ui/gestures";
 import { Color } from 'color';
 import * as enums from "ui/enums";
+import * as SocialShare from "nativescript-social-share";
+import { ImageSource, fromUrl } from "image-source";
 
 @Component({
   selector: 'app-dishdetail',
@@ -41,13 +43,14 @@ export class DishdetailComponent implements OnInit {
 
   constructor(private dishservice: DishService,
     private favoriteservice: FavoriteService,
-    private modalService: ModalDialogService, 
+    private modalService: ModalDialogService,
     private vcRef: ViewContainerRef,
     private fonticon: TNSFontIconService,
     private route: ActivatedRoute,
     private routerExtensions: RouterExtensions,
     private page: Page,
     @Inject('BaseURL') private BaseURL) { }
+
 
   ngOnInit() {
 
@@ -69,7 +72,7 @@ export class DishdetailComponent implements OnInit {
     if (!this.favorite) {
       console.log('Adding to Favorites', this.dish.id);
       this.favorite = this.favoriteservice.addFavorite(this.dish.id);
-      const toast = new Toasty("Added Dish "+ this.dish.id, "short", "bottom");
+      const toast = new Toasty("Added Dish " + this.dish.id, "short", "bottom");
       toast.show();
     }
   }
@@ -78,33 +81,48 @@ export class DishdetailComponent implements OnInit {
     this.routerExtensions.back();
   }
 
-  moreActions(): void {
+  socialShare() {
+    let image: ImageSource;
+
+    fromUrl(this.BaseURL + this.dish.image)
+     .then((img: ImageSource) => {
+       image = img; 
+        SocialShare.shareImage(image, "How would you like to share this image?")
+      })
+     .catch(()=> { console.log('Error loading image'); });
+
+  }
+
+  displayActionDialog(): void {
     let options = {
       title: "Additionnal Actions",
       cancelButtonText: "Cancel",
-      actions: ["Add to Favorites", "Add Comment"]
-  };
-  
-  action(options).then((result) => {
-      if(result == "Add to Favorites"){
+      actions: ["Add to Favorites", "Add Comment", "Social Sharing"]
+    };
+
+    action(options).then((result) => {
+      if (result == "Add to Favorites") {
         this.addToFavorites();
       }
-      else if(result == "Add Comment"){
+      else if (result == "Add Comment") {
         let options: ModalDialogOptions = {
           viewContainerRef: this.vcRef,
           fullscreen: false
-      };
+        };
 
-      this.modalService.showModal(CommentComponent, options)
-      .then((result: Comment) => {
-        console.log(JSON.stringify(result));
-        this.dish.comments.push(result);
-        this.avgstars = (((Number(this.avgstars) * this.numcomments) + result.rating) / (this.numcomments + 1)).toFixed(2);
-        this.numcomments += 1;
-      });
+        this.modalService.showModal(CommentComponent, options)
+          .then((result: Comment) => {
+            console.log(JSON.stringify(result));
+            this.dish.comments.push(result);
+            this.avgstars = (((Number(this.avgstars) * this.numcomments) + result.rating) / (this.numcomments + 1)).toFixed(2);
+            this.numcomments += 1;
+          });
 
       }
-  });
+      else if (result === 'Social Sharing') {
+        this.socialShare();
+      }
+    });
   }
 
   onSwipe(args: SwipeGestureEventData) {
@@ -114,10 +132,10 @@ export class DishdetailComponent implements OnInit {
       this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
       this.commentList = <View>this.page.getViewById<View>("commentList");
 
-      if (args.direction === SwipeDirection.up && !this.showComments ) {
+      if (args.direction === SwipeDirection.up && !this.showComments) {
         this.animateUp();
       }
-      else if (args.direction === SwipeDirection.down && this.showComments ) {
+      else if (args.direction === SwipeDirection.down && this.showComments) {
         this.showComments = false;
         this.animateDown();
       }
@@ -126,36 +144,36 @@ export class DishdetailComponent implements OnInit {
   }
 
   showAndHideComments() {
-      this.cardImage = <View>this.page.getViewById<View>("cardImage");
-      this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
-      this.commentList = <View>this.page.getViewById<View>("commentList");
+    this.cardImage = <View>this.page.getViewById<View>("cardImage");
+    this.cardLayout = <View>this.page.getViewById<View>("cardLayout");
+    this.commentList = <View>this.page.getViewById<View>("commentList");
 
-      if (!this.showComments ) {
-        this.animateUp();
-      }
-      else if (this.showComments ) {
-        this.showComments = false;
-        this.animateDown();
-      }
+    if (!this.showComments) {
+      this.animateUp();
+    }
+    else if (this.showComments) {
+      this.showComments = false;
+      this.animateDown();
+    }
   }
 
   animateUp() {
     let definitions = new Array<AnimationDefinition>();
     let a1: AnimationDefinition = {
-        target: this.cardImage,
-        scale: { x: 1, y: 0 },
-        translate: { x: 0, y: -200 },
-        opacity: 0,
-        duration: 500,
-        curve: enums.AnimationCurve.easeIn
+      target: this.cardImage,
+      scale: { x: 1, y: 0 },
+      translate: { x: 0, y: -200 },
+      opacity: 0,
+      duration: 500,
+      curve: enums.AnimationCurve.easeIn
     };
     definitions.push(a1);
 
     let a2: AnimationDefinition = {
-        target: this.cardLayout,
-        backgroundColor: new Color("#ffc107"),
-        duration: 500,
-        curve: enums.AnimationCurve.easeIn
+      target: this.cardLayout,
+      backgroundColor: new Color("#ffc107"),
+      duration: 500,
+      curve: enums.AnimationCurve.easeIn
     };
     definitions.push(a2);
 
@@ -164,28 +182,28 @@ export class DishdetailComponent implements OnInit {
     animationSet.play().then(() => {
       this.showComments = true;
     })
-    .catch((e) => {
+      .catch((e) => {
         console.log(e.message);
-    });
-  } 
+      });
+  }
 
   animateDown() {
     let definitions = new Array<AnimationDefinition>();
     let a1: AnimationDefinition = {
-        target: this.cardImage,
-        scale: { x: 1, y: 1 },
-        translate: { x: 0, y: 0 },
-        opacity: 1,
-        duration: 500,
-        curve: enums.AnimationCurve.easeIn
+      target: this.cardImage,
+      scale: { x: 1, y: 1 },
+      translate: { x: 0, y: 0 },
+      opacity: 1,
+      duration: 500,
+      curve: enums.AnimationCurve.easeIn
     };
     definitions.push(a1);
 
     let a2: AnimationDefinition = {
-        target: this.cardLayout,
-        backgroundColor: new Color("#ffffff"),
-        duration: 500,
-        curve: enums.AnimationCurve.easeIn
+      target: this.cardLayout,
+      backgroundColor: new Color("#ffffff"),
+      duration: 500,
+      curve: enums.AnimationCurve.easeIn
     };
     definitions.push(a2);
 
@@ -193,8 +211,8 @@ export class DishdetailComponent implements OnInit {
 
     animationSet.play().then(() => {
     })
-    .catch((e) => {
+      .catch((e) => {
         console.log(e.message);
-    });
+      });
   }
 }
